020882601b53ce1cd955691ce0b58ea788405de9
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2024-12-12 11:23:38 -0800

    Add files via upload

---
+++ build/_app/immutable/entry/start.BUTfpzqQ.js
@@ -0,0 +1,1 @@
+import{s as t}from"../chunks/entry.CG5qz6D8.js";export{t as start};
---
+++ build/_app/immutable/nodes/3.8jEN_J6U.js
@@ -0,0 +1,7 @@
+import{f as p,k as d,s as e,a as g,t as h,l as c}from"../chunks/disclose-version.BcNZmKmL.js";import{s as n}from"../chunks/attributes.CuqHSJQM.js";const f=""+new URL("../assets/myFace.C1997zoo.jpg",import.meta.url).href,u=""+new URL("../assets/nature1.CB9CZlpn.jpg",import.meta.url).href;var v=h(`<div class="head_Line"><h1>About:</h1></div> <img class="face svelte-1s7dzov" alt="its my face"> <h1>Joel Rivers</h1> <p>I picked up website development a few years ago. While working in a retail store. I spent down
+	time learning about building a new website for the shop. Something magic happened and I easily
+	found myself addicted. One realm of learning bled into another. As the wealth of knowledge grew,
+	so did the interest in learning more. I've done it for fun, but I want to use my skills to help
+	people who need it.</p> <p>I do spend some time with fun things like going to concerts or playing video games. To get away
+	and reset the brain. Hiking and backpacking is another nice escape from being immersed in
+	technology. There is something magic living in the wild serenity of the Pacific Northwest.</p> <img alt="a sunny day on top of a mountain at a lake. It is serene" class="svelte-1s7dzov">`,1);function y(s){var a=v(),t=p(a);d(t),c(t);var i=e(e(t,!0));n(i,"src",f);var o=e(e(i,!0)),r=e(e(o,!0)),l=e(e(r,!0)),m=e(e(l,!0));n(m,"src",u),g(s,a)}export{y as component};
