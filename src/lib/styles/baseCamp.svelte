38e01d438849c6d8ef35fe8350f228ec4ea549cc
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2024-12-12 11:55:07 -0800

    Delete src directory

--- src/lib/styles/baseCamp.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import './animation.css'
-	import './colors.css'
-	import './components.css'
-	import './buttons.css'
-	import './generals.css'
-	import './vari.css'
-	// import './util-First.css'
-</script>
