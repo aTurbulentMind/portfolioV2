5aef4c38373f266ea0af7c50ff1e91c3581d2486
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2024-12-17 13:40:02 -0800

    Add files via upload

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,15 @@
-// src/hooks.server.ts
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
-import { createServerClient } from '@supabase/ssr';
-import type { Handle } from '@sveltejs/kit';
+import { createServerClient } from '@supabase/ssr'
+import { type Handle, redirect } from '@sveltejs/kit'
+import { sequence } from '@sveltejs/kit/hooks'
 
-export const handle: Handle = async ({ event, resolve }) => {
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
+
+const supabase: Handle = async ({ event, resolve }) => {
+	/**
+	 * Creates a Supabase client specific to this server request.
+	 *
+	 * The Supabase client gets the Auth token from the request cookies.
+	 */
 	event.locals.supabase = createServerClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
 		cookies: {
 			getAll: () => event.cookies.getAll(),
@@ -14,11 +20,14 @@ export const handle: Handle = async ({ event, resolve }) => {
 			 */
 			setAll: (cookiesToSet) => {
 				cookiesToSet.forEach(({ name, value, options }) => {
-					event.cookies.set(name, value, { ...options, path: '/' });
-				});
+					event.cookies.set(name, value, {
+						...options,
+						path: '/'
+					})
+				})
 			}
 		}
-	});
+	})
 
 	/**
 	 * Unlike `supabase.auth.getSession()`, which returns the session _without_
@@ -28,26 +37,49 @@ export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.safeGetSession = async () => {
 		const {
 			data: { session }
-		} = await event.locals.supabase.auth.getSession();
+		} = await event.locals.supabase.auth.getSession()
 		if (!session) {
-			return { session: null, user: null };
+			return { session: null, user: null }
 		}
 
 		const {
 			data: { user },
 			error
-		} = await event.locals.supabase.auth.getUser();
+		} = await event.locals.supabase.auth.getUser()
 		if (error) {
 			// JWT validation has failed
-			return { session: null, user: null };
+			return { session: null, user: null }
 		}
 
-		return { session, user };
-	};
+		return { session, user }
+	}
 
 	return resolve(event, {
 		filterSerializedResponseHeaders(name) {
-			return name === 'content-range' || name === 'x-supabase-api-version';
+			/**
+			 * Supabase libraries use the `content-range` and `x-supabase-api-version`
+			 * headers, so we need to tell SvelteKit to pass it through.
+			 */
+			return name === 'content-range' || name === 'x-supabase-api-version'
 		}
-	});
-};
+	})
+}
+
+const authGuard: Handle = async ({ event, resolve }) => {
+	const { session, user } = await event.locals.safeGetSession()
+
+	event.locals.session = session
+	event.locals.user = user
+
+	if (!event.locals.session && event.url.pathname.startsWith('/app')) {
+		redirect(303, '/login')
+	}
+
+	if (event.locals.session && event.url.pathname.startsWith('/login')) {
+		redirect(303, '/app/dashboard')
+	}
+
+	return resolve(event)
+}
+
+export const handle: Handle = sequence(supabase, authGuard)
