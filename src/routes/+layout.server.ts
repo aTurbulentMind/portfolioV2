174f48581ba111a9858a04e5ec91cd8ffd5d01d8
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2024-12-17 13:25:30 -0800

    Add files via upload

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,18 +2,10 @@
 import type { LayoutServerLoad } from './$types'
 
 export const load: LayoutServerLoad = async ({ locals: { safeGetSession }, cookies }) => {
-	const { user } = await safeGetSession()
-
-	// Example: Set a cookie for theme or session
-	cookies.set('siteTheme', 'dark', {
-		path: '/', // Available on all routes
-		httpOnly: true, // Not accessible via JavaScript
-		secure: true, // Send only over HTTPS
-		sameSite: 'lax', // Avoid CSRF
-		maxAge: 60 * 60 * 24 * 7 // Cookie lifespan: 7 days in seconds
-	})
+	const { session, user } = await safeGetSession()
 
 	return {
+		session,
 		user,
 		cookies: cookies.getAll()
 	}
