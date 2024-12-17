3201a760cd08a50325b5366961a46cd0e5a55aa7
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2024-12-17 14:19:18 -0800

    Add files via upload

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,22 +1,39 @@
-// src/hooks.server.ts
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
 import { createServerClient } from '@supabase/ssr'
-import type { Handle } from '@sveltejs/kit'
+import { type Handle, redirect } from '@sveltejs/kit'
+import { sequence } from '@sveltejs/kit/hooks'
 
-export const handle: Handle = async ({ event, resolve }) => {
-	// Set up Supabase client
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
+
+const supabase: Handle = async ({ event, resolve }) => {
+	/**
+	 * Creates a Supabase client specific to this server request.
+	 *
+	 * The Supabase client gets the Auth token from the request cookies.
+	 */
 	event.locals.supabase = createServerClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
 		cookies: {
 			getAll: () => event.cookies.getAll(),
+			/**
+			 * SvelteKit's cookies API requires `path` to be explicitly set in
+			 * the cookie options. Setting `path` to `/` replicates previous/
+			 * standard behavior.
+			 */
 			setAll: (cookiesToSet) => {
 				cookiesToSet.forEach(({ name, value, options }) => {
-					event.cookies.set(name, value, { ...options, path: '/' })
+					event.cookies.set(name, value, {
+						...options,
+						path: '/'
+					})
 				})
 			}
 		}
 	})
 
-	// Safe session getter method
+	/**
+	 * Unlike `supabase.auth.getSession()`, which returns the session _without_
+	 * validating the JWT, this function also calls `getUser()` to validate the
+	 * JWT before returning the session.
+	 */
 	event.locals.safeGetSession = async () => {
 		const {
 			data: { session }
@@ -37,15 +54,32 @@ export const handle: Handle = async ({ event, resolve }) => {
 		return { session, user }
 	}
 
-	// Get theme from cookies
-	const theme = event.cookies.get('siteTheme')
-
-	// Single resolve call with theme transformation
 	return resolve(event, {
 		filterSerializedResponseHeaders(name) {
+			/**
+			 * Supabase libraries use the `content-range` and `x-supabase-api-version`
+			 * headers, so we need to tell SvelteKit to pass it through.
+			 */
 			return name === 'content-range' || name === 'x-supabase-api-version'
-		},
-		transformPageChunk: ({ html }) =>
-			html.replace('data-theme="', `data-theme="${theme || 'light'}" `)
+		}
 	})
 }
+
+const authGuard: Handle = async ({ event, resolve }) => {
+	const { session, user } = await event.locals.safeGetSession()
+
+	event.locals.session = session
+	event.locals.user = user
+
+	if (!event.locals.session && event.url.pathname.startsWith('/app')) {
+		redirect(303, '/login')
+	}
+
+	if (event.locals.session && event.url.pathname.startsWith('/login')) {
+		redirect(303, '/app/dashboard')
+	}
+
+	return resolve(event)
+}
+
+export const handle: Handle = sequence(supabase, authGuard)
