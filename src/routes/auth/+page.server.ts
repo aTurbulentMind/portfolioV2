dca6b1622d0b82cdc8db95c0f9aced94f69da7dd
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2025-01-03 15:40:51 -0800

    Add files via upload

--- src/routes/auth/+page.server.ts
+++ src/routes/auth/+page.server.ts
@@ -1,32 +1,46 @@
-import { redirect } from '@sveltejs/kit';
+// src/routes/+page.server.ts
+import { fail, redirect } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
 
-import type { Actions } from './$types';
+export const load: PageServerLoad = async ({ url, locals: { safeGetSession } }) => {
+	const { session } = await safeGetSession();
+
+	// if the user is already logged in return them to the account page
+	if (session) {
+		redirect(303, '/account');
+	}
+
+	return { url: url.origin };
+};
 
 export const actions: Actions = {
-	signup: async ({ request, locals: { supabase } }) => {
+	default: async (event) => {
+		const {
+			url,
+			request,
+			locals: { supabase }
+		} = event;
 		const formData = await request.formData();
 		const email = formData.get('email') as string;
-		const password = formData.get('password') as string;
+		const validEmail = /^[\w-\.+]+@([\w-]+\.)+[\w-]{2,8}$/.test(email);
 
-		const { error } = await supabase.auth.signUp({ email, password });
-		if (error) {
-			console.error(error);
-			redirect(303, '/auth/error');
-		} else {
-			redirect(303, '/');
+		if (!validEmail) {
+			return fail(400, { errors: { email: 'Please enter a valid email address' }, email });
 		}
-	},
-	login: async ({ request, locals: { supabase } }) => {
-		const formData = await request.formData();
-		const email = formData.get('email') as string;
-		const password = formData.get('password') as string;
 
-		const { error } = await supabase.auth.signInWithPassword({ email, password });
+		const { error } = await supabase.auth.signInWithOtp({ email });
+
 		if (error) {
-			console.error(error);
-			redirect(303, '/auth/error');
-		} else {
-			redirect(303, '/private');
+			return fail(400, {
+				success: false,
+				email,
+				message: `There was an issue, Please contact support.`
+			});
 		}
+
+		return {
+			success: true,
+			message: 'Please check your email for a magic link to log into the website.'
+		};
 	}
 };
